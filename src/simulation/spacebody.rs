4b74c949085a52ae54996b9034f07f0ac7a7ff91
FeistyKit <eeveebeevee33@gmail.com>
2021-06-14 14:42:45 -0400

    removed soa and implemented rayon

--- src/simulation/spacebody.rs
+++ src/simulation/spacebody.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, StructOfArray, Serialize, Deserialize)]
-#[soa_derive = "Debug"]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct SpaceBody {
     pub x: f32,
     pub y: f32,
--- src/simulation/worldspace.rs
+++ src/simulation/worldspace.rs
@@ -1,5 +1,5 @@
 #![allow(clippy::explicit_counter_loop)]
-use std::sync::mpsc::Sender;
+use std::sync::{mpsc::Sender, Arc, Mutex};
 
 use sfml::system::Vector2f;
 
@@ -9,40 +9,41 @@ use rayon::prelude::*;
 
 #[allow(unused)]
 pub struct WorldSpace {
-    bodies: SpaceBodyVec,
+    bodies: Vec<SpaceBody>,
     highlighted_id: Option<usize>,
 }
 impl WorldSpace {
     #[allow(unused)]
     pub fn new() -> WorldSpace {
         WorldSpace {
-            bodies: SpaceBodyVec::new(),
+            bodies: Vec::new(),
             highlighted_id: None,
         }
     }
-    fn update_positions(&mut self, sender: &mut Sender<SimulationEvent>, dt: f32) {
+    fn update_positions(&mut self, sender: Sender<SimulationEvent>, dt: f32) {
         //updating positions of each body
-        let mut idx = 0;
-        for (x, y, xv, yv, id) in soa_zip!(&mut self.bodies, [mut x, mut y, xv, yv, id]) {
-            //this is so the computer doesn't have to drudge up every field of the struct when it doesn't need to. You'll see this a lot
-            *x += xv * dt;
-            *y += yv * dt;
-            sender
-                .send(SimulationEvent::Move {
-                    id: *id,
-                    idx,
-                    pos: Vector2f::new(*x, *y),
-                    change: Vector2f::new(*xv, *yv),
+        self.bodies
+            .par_iter_mut()
+            .enumerate()
+            .for_each_with(sender, |sen, tuple| {
+                //so that each can send to the other thread
+                let body = tuple.1;
+                sen.send(SimulationEvent::Move {
+                    id: body.id,
+                    idx: tuple.0,
+                    pos: Vector2f::new(body.x, body.y),
+                    change: Vector2f::new(body.xv, body.yv),
                 })
                 .unwrap();
-            idx += 1;
-        }
+                body.x += body.xv * dt;
+                body.y += body.yv * dt;
+            });
     }
     fn update_velocity(&mut self, dt: f32) {
-        for (xv, yv, ax, ay) in soa_zip!(&mut self.bodies, [mut xv, mut yv, ax, ay]) {
-            *xv += *ax * dt;
-            *yv += *ay * dt;
-        }
+        self.bodies.par_iter_mut().for_each(|body| {
+            body.xv += body.ax;
+            body.yv += body.ay;
+        });
     }
     fn update_accelerations(&mut self, g: f32, softening: f32) {
         let mut ax = 0;
